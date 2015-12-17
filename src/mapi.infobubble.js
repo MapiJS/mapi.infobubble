8a0d479d5b67842843b539e5dfb073859bcacc4e
Thiago Felipe Ribeiro <tribeiro@sorocaba.sp.gov.br>
2015-12-17 16:20:19 -0200

    First code commit.

---
+++ src/mapi.infobubble.js
@@ -0,0 +1,57 @@
+var _ = require('underscore');
+
+require('js-info-bubble');
+
+let MapInfoBubble = {
+	infoBubble: InfoBubble,
+	addInfoBubble({
+		className = 'infobubble', 
+		id = _.uniqueId('infobubble'), 
+		content,
+		shadowStyle = 0,
+		padding = 5,
+		borderRadius = 10,
+		borderWidth = 2,
+		borderColor = '#2c2c2c',
+		hideCloseButton = false,
+		maxHeight = 140,
+		maxWidth = 200,
+		disableAutoPan = true,
+		divToPlot = '', 
+		...options
+	}) {
+		options = {
+			className , 
+			id , 
+			content,
+			shadowStyle,
+			padding,
+			borderRadius,
+			borderWidth,
+			borderColor,
+			hideCloseButton,
+			maxHeight,
+			maxWidth,
+			disableAutoPan,
+			divToPlot, 
+			...options
+		}
+
+		options.map = this.map;
+
+		if (options.lat && options.lng) {
+			options.position = new google.maps.LatLng(options.lat, options.lng);
+		}
+
+		var obj;
+		obj = new this.infoBubble(options);
+		
+		obj.open();
+
+		this.addObject(className, id, obj);
+
+		return obj;
+	}
+}
+InfoBubble = null;
+Module.exports = MapInfoBubble;
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var webpack = require('webpack');
+var pkg = require('./package.json');
+
+module.exports = {
+	output: {
+		libraryTarget: 'umd',
+		library: 'MapiInfoBubble'
+	},
+	externals: {
+        "underscore": {
+        	root: "_",
+        	commonjs: "underscore",
+        	commonjs2: "underscore",
+        	amd: "underscore"
+        }
+    },
+	module: {		
+		preLoaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'eslint-loader'
+			}
+		],
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015'],
+					plugins: ['transform-object-rest-spread'],
+					cacheDirectory: true
+				}
+			}
+		]
+	},
+	plugins: [
+		new webpack.BannerPlugin([
+				pkg.name[0].toUpperCase() + pkg.name.slice(1) + ' - ' + pkg.description,
+				'Version: ' + pkg.version,
+				'Author: ' + pkg.author
+			].join('\n'), {entryOnly: true})
+	],
+	eslint: {
+		parser: "babel-eslint"
+		
+	}
+};
